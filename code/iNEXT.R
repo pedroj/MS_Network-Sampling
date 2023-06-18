6d02e99deee357d1687f6ee589811bae25f49555
Pedro Jordano <jordano@ebd.csic.es>
2023-06-18 13:21:50 +0200

    Updated iNEXT code.

--- code/iNEXT.R
+++ code/iNEXT.R
@@ -1,15 +1,23 @@
+# Sampling completeness analysis with iNEXT.link.
+# Sevilla, 16 Jun 2023.
+# 
+library(here)
+library(iNEXT.link)
 
-### NAVA CORREHUELAS
-nchq<-as.matrix(read.table(here("data/nchq.txt"),header=F,sep="\t",dec=".",na.strings="NA"))
+### Nava Correhuelas
+nchq<- read.table(here("data/nchq.txt"), header=F, sep="\t", dec=".", na.strings="NA")
 
-### HATO RATON
-hrq<-as.matrix(read.table(here("data/hrq.txt"),header=F,sep="\t",dec=".",na.strings="NA"))
+### Hato Raton
+hrq<- read.table(here("data/hrq.txt"), header=F, sep="\t", dec=".", na.strings="NA")
 
-### ZACKENBERG
-zackq<-as.matrix(read.table(here("data/zackq.txt"),header=F,sep="\t",dec=".",na.strings="NA"))
+### Nava Noguera
+nnogq<- read.table(here("data/sdw03_adj_vis.csv"), header=F,sep=",",dec=".",na.strings="NA")
+
+### Zackenberg
+zackq<- read.table(here("data/zackq.txt"),header=F,sep="\t",dec=".",na.strings="NA")
 
 # Create list of data frame using list()
-listOfDataframe = list(nch= t(nchq), hr= t(hrq), zack= t(zackq))
+listOfDataframe = list(nch= t(nchq), hr= t(hrq), nnog= nnogq, zack= t(zackq))
 
 ## Taxonomic diversity
 output1 = AO.link(data = listOfDataframe, diversity = 'TD', q = seq(0, 2, 0.2))
