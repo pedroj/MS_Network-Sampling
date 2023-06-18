6b0844969b1f1632da27ea6caff760ea5bad1ee7
Pedro Jordano <jordano@ebd.csic.es>
2023-06-18 11:25:15 +0200

    Updated iNEXT code.

--- code/iNEXT.R
+++ code/iNEXT.R
@@ -9,7 +9,7 @@ hrq<-as.matrix(read.table(here("data/hrq.txt"),header=F,sep="\t",dec=".",na.stri
 zackq<-as.matrix(read.table(here("data/zackq.txt"),header=F,sep="\t",dec=".",na.strings="NA"))
 
 # Create list of data frame using list()
-listOfDataframe = list(nch= nchq, hr=hrq, zack= zackq)
+listOfDataframe = list(nch= t(nchq), hr= t(hrq), zack= t(zackq))
 
 ## Taxonomic diversity
 output1 = AO.link(data = listOfDataframe, diversity = 'TD', q = seq(0, 2, 0.2))
@@ -43,4 +43,11 @@ outcome<- Completeness.link(
     nboot = 30)
 
 ggCompleteness.link(outcome)
+
+output1 <- estimateD.link(listOfDataframe, diversity = 'TD', 
+                          base = "coverage", level = 0.7, nboot = 30)
+output1
+
+output1 = iNEXTbeta.link(data = listOfDataframe, diversity = 'TD', level = seq(0.5, 0.9, 0.4), q = c(0, 1, 2))
+output1
 
